b8040957c87cc94d44ae32f3fa7e55a75be13204
Mikhail <i@MkRusakov.ru>
2022-09-26 19:04:26 +0300

    Task2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,12 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 def function (X, Y, Z):
-    print(not (X or Y or Z) == (not X and not Y and not Z))
-function(1, 2, 3)
-function(3, 2, 1)
-function(2, 2, 2)
-
+    print(not (X or Y or Z) == ((not X) and (not Y) and (not Z)))
+function(10, -4, 3)
+function(10, 4, -7)
+function(-8, 2, 6)
+function(8, 2, 6)
+function(-8, -2, -6)
+function(8, 8, 8)
+function(-8, -8, -8)
+function(0, 0, 0)
+# И это правильно? Что-то я не совсем понял ТЗ
