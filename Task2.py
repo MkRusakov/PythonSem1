6f0d3bf2da0329a39b76a7c4bb646ac3c5f21c29
Mikhail <i@MkRusakov.ru>
2022-09-27 10:53:44 +0300

    Task2

--- Task2.py
+++ Task2.py
@@ -1,12 +1,5 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+import random
 def function (X, Y, Z):
     print(not (X or Y or Z) == ((not X) and (not Y) and (not Z)))
-function(10, -4, 3)
-function(10, 4, -7)
-function(-8, 2, 6)
-function(8, 2, 6)
-function(-8, -2, -6)
-function(8, 8, 8)
-function(-8, -8, -8)
-function(0, 0, 0)
-# И это правильно? Что-то я не совсем понял ТЗ
+function(random.choice([True, False]), random.choice([True, False]), random.choice([True, False]))
